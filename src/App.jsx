6885fa062d568fae1d11c72403794c21e24905ae
Jacob McGreen <119445109+jacobmcgreen@users.noreply.github.com>
2024-01-20 18:40:04 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
 		>
 			{theme === 'dark' ? sun : moon}
 		</button>
-		<div className="bg-sky-500 dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
+		<div className="bg-emerald-300 dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
 			<div className="max-w-5xl w-11/12 mx-auto">
 				<Intro />
 				<Portfolio />
