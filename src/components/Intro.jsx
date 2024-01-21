19a495a17824cc3a0cae4637bb00366e9e53108c
Jacob McGreen <119445109+jacobmcgreen@users.noreply.github.com>
2024-01-21 16:04:59 -0500

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,7 @@ function Intro() {
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
          <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Jacob McGreen</h1>
          <p className="text-base md:text-xl mb-3 font-medium">Computer Science Student and Aspiring Software Developer</p>
-         <p className="text-sm max-w-xl mb-6 font-bold"></p>
+         <p className="text-sm max-w-xl mb-6 font-bold">Sophmore at the University of Rochester</p>
       </div>
    )
 }
