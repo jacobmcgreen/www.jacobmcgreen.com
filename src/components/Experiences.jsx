896bc15c192bfb20da385d1ce7af16e246a6feff
Jacob McGreen <119445109+jacobmcgreen@users.noreply.github.com>
2024-05-08 21:37:40 -0500

    testing expiriences page

---
+++ src/components/Experiences.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Experiences = () => {
+  return (
+    <div>
+      <h1>My Experiences</h1>
+      <p>This is the Experiences page where you can find my professional background.</p>
+    </div>
+  );
+};
+
+export default Experiences;
