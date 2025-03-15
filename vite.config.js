e6bca65e7ced20d429481ca943a7e30b01cd55aa
jacobmcgreen <119445109+jacobmcgreen@users.noreply.github.com>
2025-03-15 14:20:53 -0400

    updating for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/www.jacobmcgreen.com',
+  base: '/www.jacobmcgreen.com/',
 })
